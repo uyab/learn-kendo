b8e1f8d7c7aecfc490843816a7fa331dfaf5f473
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2018-05-07 10:49:49 +0700

    bare minimum HTML with Kendo UI
    Style default dari element HTML tidak otomatis dioverride ketika
    meng-include Kendo CSS

--- index.php
+++ index.php
@@ -6,8 +6,111 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kendo UI from scratch to catch</title>
+
+    <!-- Common Kendo UI CSS for web widgets and widgets for data visualization. -->
+    <link href="styles/kendo.common.min.css" rel="stylesheet" type="text/css"/>
+
+    <!-- Default Kendo UI theme CSS for web widgets and widgets for data visualization. -->
+    <link href="styles/kendo.default.min.css" rel="stylesheet" type="text/css"/>
+
+    <!-- jQuery JavaScript -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Kendo UI combined JavaScript -->
+    <script src="js/kendo.web.min.js"></script>
+
 </head>
 <body>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure nesciunt pariatur quas reiciendis sequi! Alias dicta eius error esse et eveniet, expedita labore omnis pariatur reprehenderit ut velit voluptatibus voluptatum?</p>
+
+
+<a>Anchor</a>
+<em>Emphasis</em>
+<small>Small</small>
+<strong>Strong</strong>
+<u>Underline</u>
+
+
+<h1>Heading</h1>
+<h2>Heading</h2>
+<h3>Heading</h3>
+<h4>Heading</h4>
+<h5>Heading</h5>
+<h6>Heading</h6>
+
+<blockquote>
+    <p><em>Yeah!! Milligram is amazing.</em></p>
+</blockquote>
+
+<ul>
+    <li>Unordered list item 1</li>
+    <li>Unordered list item 2</li>
+</ul>
+
+
+<ol>
+    <li>Ordered list item 1</li>
+    <li>Ordered list item 2</li>
+</ol>
+
+
+<dl>
+    <dt>Description list item 1</dt>
+    <dd>Description list item 1.1</dd>
+</dl>
+
+<form>
+    <fieldset>
+        <label for="nameField">Name</label>
+        <input type="text" placeholder="CJ Patoilo" id="nameField">
+        <label for="ageRangeField">Age Range</label>
+        <select id="ageRangeField">
+            <option value="0-13">0-13</option>
+            <option value="14-17">14-17</option>
+            <option value="18-23">18-23</option>
+            <option value="24+">24+</option>
+        </select>
+        <label for="commentField">Comment</label>
+        <textarea placeholder="Hi CJ …" id="commentField"></textarea>
+        <div class="float-right">
+            <input type="checkbox" id="confirmField">
+            <label class="label-inline" for="confirmField">Send a copy to yourself</label>
+        </div>
+        <input class="button-primary" type="submit" value="Send">
+    </fieldset>
+</form>
+
+<table>
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Height</th>
+        <th>Location</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Stephen Curry</td>
+        <td>27</td>
+        <td>1,91</td>
+        <td>Akron, OH</td>
+    </tr>
+    <tr>
+        <td>Klay Thompson</td>
+        <td>25</td>
+        <td>2,01</td>
+        <td>Los Angeles, CA</td>
+    </tr>
+    </tbody>
+</table>
+
+<pre><code>
+.milligram {
+  color: #9b4dca;
+}
+</code></pre>
 
 </body>
 </html>
