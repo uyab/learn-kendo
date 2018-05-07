5f7b3b908f738995c8003ba5b95374b189314169
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2018-05-07 11:41:37 +0700

    Basic form menggunakan skin default v2

---
+++ form.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kendo UI from scratch to catch</title>
+
+    <!-- Common Kendo UI CSS for web widgets and widgets for data visualization. -->
+    <link href="styles/kendo.common.min.css" rel="stylesheet" type="text/css"/>
+
+    <!-- Default Kendo UI theme CSS for web widgets and widgets for data visualization. -->
+    <link href="styles/kendo.default-v2.min.css" rel="stylesheet" type="text/css"/>
+
+    <!-- jQuery JavaScript -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Kendo UI combined JavaScript -->
+    <script src="js/kendo.web.min.js"></script>
+
+</head>
+<body>
+
+<form class="k-form">
+    <label class="k-form-field">
+        <span>First Name</span>
+        <input class="k-textbox" placeholder="Your Name" />
+    </label>
+    <label class="k-form-field">
+        <span>Last Name</span>
+        <input class="k-textbox" placeholder="Your Last Name" />
+    </label>
+
+    <div class="k-form-field">
+        <button class="k-button">Default</button>&nbsp;
+        <button class="k-button k-primary">Primary</button>
+    </div>
+    
+</form>
+
+</body>
+</html>
